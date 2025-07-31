2716cf3f3f81a8f9772e3c206ca44df698e0073a
Hiroto-Aizawa <r_20love@icloud.com>
2025-08-01 02:38:47 +0900

    Chapter7
    メモの削除と更新

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     # トップページのURLパスで、views.py内のindex関数を呼び出す
     # url, view, url_name
     path('', views.index, name='index'),
-    path('<int:memo_id>/', views.detail, name='detail'),
+    path('<int:memo_id>', views.detail, name='detail'),
     path('new_memo', views.new_memo, name='new_memo'),
+    path('delete_memo/<int:memo_id>', views.delete_memo, name='delete_memo'),
+    path('edit_memo/<int:memo_id>', views.edit_memo, name='edit_memo'),
 ]
